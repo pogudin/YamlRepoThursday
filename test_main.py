b65f80e0dbaa3500b382edc99d9d8fed5304c887
Ivan <ivan.pogudin@gmail.com>
2025-09-18 14:18:25 -0500

    Added tests to test output of hello() and bye()

--- test_main.py
+++ test_main.py
@@ -1,9 +1,21 @@
 from goodhello import hello, bye
 
 def test_hello_returns_none():
-    assert hello() is 1
+    assert hello() is None:
 
 def test_bye_returns_none():
     assert bye() is None
+
+def test_hello_prints_expected_test(capsys):
+    hello()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Hello DevOps!"
+
+def test_bye_prints_expected_test(capsys):
+    bye()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Good bye!"
+
+
 
 
